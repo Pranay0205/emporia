ac02c8de16818951c864dc31648a80a8dbe5f250
Pranay Ghuge <35730341+Pranay0205@users.noreply.github.com>
2025-04-03 11:19:33 -0400

    feat(database): add MySQL support

---
+++ emporia-api/dal/db_service.py
@@ -0,0 +1,37 @@
+import configparser
+import mysql.connector
+
+class DatabaseConnection:
+  _db_instance = None
+  
+  def __new__(cls, *args, **kwargs):
+    if not cls._db_instance:
+      cls._db_instance = super(DatabaseConnection, cls).__new__(cls)
+      cls._db_instance.connection = None
+      cls._db_instance.cursor = None
+      cls._db_instance._connect()
+    return cls._db_instance
+  
+  def _connect(self):
+    try:
+      config = configparser.ConfigParser()
+      config.read('configs/config.ini')
+
+      self.connection = mysql.connector.connect(
+        host=config['database']['host'],
+        user=config['database']['user'],
+        password=config['database']['password'],
+        database=config['database']['database']
+      )
+      self.cursor = self.connection.cursor()
+      print("Database connection established.")
+    except mysql.connector.Error as err:
+      print(f"Error: {err}")
+      self.connection = None
+      self.cursor = None      
+    except configparser.Error as err:
+      print(f"Error reading configuration file: {err}")
+      
+
+
+db = DatabaseConnection()
