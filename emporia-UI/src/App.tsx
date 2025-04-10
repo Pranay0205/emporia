a11c3d9897c636647655a16139568d500dfbf42b
Arundhati Das <arundhutidas2000@gmail.com>
2025-04-10 13:41:16 -0400

    Setup: CHakra ui

--- emporia-UI/src/App.tsx
+++ emporia-UI/src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
+import { Button, HStack } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold bg-green-700 items-center">
-        One Piece is real
-      </h1>
+      <h1 className="text-3xl font-bold bg-green-700">One Piece is real</h1>
+
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
     </>
   );
 }
