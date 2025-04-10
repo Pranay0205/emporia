a6c68ec778ddc5e58f95740562e30fc88123940e
Arundhati Das <arundhutidas2000@gmail.com>
2025-04-10 18:13:18 -0400

    feat: added RegistrationPage

--- emporia-UI/src/App.tsx
+++ emporia-UI/src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "@/components/pages/LandingPage";
+import LoginPage from "./components/pages/LoginPage";
 import { Toaster } from "@/components/ui/toaster";
+import RegistrationRouter from "./components/pages/RegistrationRouter";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegistrationRouter />} />
         {/* Add more routes here as needed */}
       </Routes>
       <Toaster />
